7d8b662177f0d77fced5fd4c012719419def8afd
Wesley Abdoul <Abdoulwes@gmail.com>
2025-01-18 20:59:06 +0100

    Réorganisation des éléments de navigation dans le composant Header et mise à jour de la configuration de base dans vite.config.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import logoKasa from "../assets/images/Logo.png";
 import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
       <img src={logoKasa} alt="Kasa Logo" />
       <nav>
         <ul>
-          <NavLink to ="/" className={({isActive}) => (isActive ? "underline" : "")}>
-          <li>Acceuil</li>
-          </NavLink>
-          <NavLink to ="about" className={({isActive}) => (isActive ? "underline" : "")}>
-          <li>A propos</li>
-          </NavLink>
+          <li>
+            <NavLink to="/" className={({ isActive }) => (isActive ? "underline" : "")}>
+              Acceuil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? "underline" : "")}>
+              A propos
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
